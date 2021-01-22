bc641609dc6a9899f89f6614e13d85332debb2b9
WillElvire <willymarley76@gmail.com>
2021-01-22 18:24:19 +0100

    inscription/auth

--- src/app/inscrire/inscrire.module.ts
+++ src/app/inscrire/inscrire.module.ts
@@ -2,22 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
-
+import { ApiService } from './../service/api.service';
 import { InscrirePageRoutingModule } from './inscrire-routing.module';
 
 import { InscrirePage } from './inscrire.page';
 
 @NgModule({
   imports: [
-    CommonModule,
+  
+
+  CommonModule,
     ReactiveFormsModule,
     FormsModule,
     RouterModule,
     IonicModule,
+    HttpClientModule,
     InscrirePageRoutingModule
   ],
-  declarations: [InscrirePage]
+  declarations: [InscrirePage],
+  providers:[ApiService]
 })
 export class InscrirePageModule {}
